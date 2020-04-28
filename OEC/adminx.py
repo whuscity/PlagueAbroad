1bba7576d94f3fc8cf0c16d85337a641a3070f49
shareHao <442667228@qq.com>
2020-04-28 11:04:02 +0800

    2020.04.28 update OECInfo Table Structure(add address,phone,url,email field)

--- OEC/adminx.py
+++ OEC/adminx.py
@@ -6,8 +6,8 @@ import pandas as pd
 from .models import OECInfo
 
 class OECInfoAdmin(object):
-    list_display = ['region', 'name', 'related_info', 'type']
-    list_filter = ['region', 'name', 'related_info', 'type']
+    list_display = ['region', 'name', 'related_info', 'address', 'phone', 'url', 'email', 'type']
+    list_filter = ['region', 'name','address', 'phone', 'url', 'email', 'type']
 
     import_csv = True
 
@@ -21,9 +21,28 @@ class OECInfoAdmin(object):
             for index, item in data_df.iterrows():
                 case = OECInfo()
                 case.id = item.get('id')
-                case.region = item.get('region')
                 case.name = item.get('name')
+                case.region = item.get('region')
                 case.related_info = item.get('related_info')
+                case.address = item.get('address')
+                case.url = item.get('url')
+                case.phone = item.get('phone')
+                case.email = item.get('email')
+                # if str(item.get('region')) == 'nan':
+                #     case.region = None
+                # if str(item.get('name')) != 'nan':
+                #     case.name = None
+                # if str(item.get('related_info')) != 'nan':
+                #     case.related_info = None
+                # if str(item.get('address')) != 'nan':
+                #     case.address = None
+                # if str(item.get('url')) != 'nan':
+                #     case.url = None
+                # if str(item.get('phone')) != 'nan':
+                #     case.phone = None
+                # if str(item.get('email')) != 'nan':
+                #     case.email = None
+
                 if item.get('type') == "驻外使馆":
                     case.type = 0
                 elif item.get('type') == "驻外总领馆":
--- OEC/models.py
+++ OEC/models.py
@@ -6,6 +6,10 @@ class OECInfo(models.Model):
     region = models.CharField(max_length=50, verbose_name="区域", null=True, blank=True)
     name = models.CharField(max_length=100, verbose_name="使馆名称", null=True, blank=True)
     related_info = models.TextField(max_length=1000, verbose_name="相关信息", null=True, blank=True)
+    address = models.CharField(max_length=200, verbose_name="地址", null=True, blank=True)
+    phone = models.CharField(max_length=100, verbose_name="电话", null=True, blank=True)
+    url = models.CharField(max_length=50, verbose_name="网址", null=True, blank=True)
+    email = models.CharField(max_length=50, verbose_name="邮箱", null=True, blank=True)
     type = models.IntegerField(choices=((0, '驻外使馆'), (1, '驻外总领馆'), (2, '驻外团、处')),
                               verbose_name="使馆类型", null=True, blank=True)
     def __str__(self):
