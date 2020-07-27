724dff8b0e83df33190fbada88a9041ee62eabe6
shareHao <442667228@qq.com>
2020-07-27 16:40:43 +0800

    2020.07.27 modify report app type field

--- report/models.py
+++ report/models.py
@@ -10,7 +10,7 @@ class Content(models.Model):
     publish_time = models.DateTimeField(verbose_name='发布时间', default=datetime.now())
     wechat_url = models.CharField(max_length=255, verbose_name='微信链接', null=True, blank=True)
     view_count = models.IntegerField(default=0, verbose_name='浏览量')
-    type = models.CharField(choices=(('R', '态势报告'), ('D', '抗疫日记')), max_length=10,
+    type = models.CharField(choices=(('R', '态势分析'), ('D', '专题报告')), max_length=10,
                               verbose_name="类型")
     is_put_top = models.IntegerField(verbose_name="是否置顶", choices=((0, '否'), (1, '是')))
 
